4467bc008d328d719fa70d45c1e406f33feb0be5
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2018-10-15 11:48:38 +0200

    Puesto bonito
    Ahora toca ponerse ya enzerio

--- index.php
+++ index.php
@@ -1,103 +1,94 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> Sulu Sport </title>
+        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/dropdown.css">
+    </head>
+    <body>
+        <header id="header">
+            <nav id="topnav">
+                <ul id="products">
+                    <a><img  src="img/logo.png"></a>
+                </ul>
+                <ul id="registreCistella">
+                    <li id="iniciaSesion">
+                        <a id="linkInicia" href="login.php"><a class="logo"><img  src="img/user.png"></a></a>
+                    </li>
+                    <a><img  src="img/cart.png"></a>
+                </ul>
+            </nav>
 
-?>
-	<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge">
-			<title> Sulu Sport </title>
-			<link rel="stylesheet" href="css/index.css">
-			<link rel="stylesheet" href="css/dropdown.css">
-		</head>
-		<body>
+        </header><!-- /header -->
 
-			<header id="header" class="">
-				<div id="topnav">
-					<ul id="products" class="arriba">
-						<li class="dropdown">FotoMarca</li>
-						<li class="dropdown">FotoProducto</li>
-						<li class="dropdown">FotoBestProducto</li>
-						<li class="dropdown">FotoOfertas</li>
-					</ul>
-					<ul id="registreCistella" class="arriba">
-						<li id="iniciaSesion" class="dropdown">
-							<a id="linkInicia" href="login.php">Entra</a>
-						</li>
-						<li id="ayuda" class="dropdown">Ayuda</li>
-						<li class="dropdown">Carrito</li>
-					</ul>
-				</div>
+        <nav class ="nav">
+            <ul>
+                <li class="dropdown">
+                    <a href="" > HOMBRE </a>
+                    <div class="dropdown-content">
+                        <a> CAMISETAS </a>
+                        <a> PANTALONES </a>
+                        <a> ZAPATILLAS </a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="" > MUJER </a>
+                    <div class="dropdown-content">
+                        <a> CAMISETAS </a>
+                        <a> PANTALONES </a>
+                        <a> ZAPATILLAS </a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="" > NIÑOS </a>
+                    <div class="dropdown-content">
+                        <a> CAMISETAS </a>
+                        <a> PANTALONES </a>
+                        <a> ZAPATILLAS </a>
+                    </div>
+                </li>
+                <li class="dropdown">
+                    <a href="" > COMPLEMENTOS </a>
+                    <div class="dropdown-content">
+                        <a> CAMISETAS </a>
+                        <a> PANTALONES </a>
+                        <a> ZAPATILLAS </a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
 
-				<nav class ="nav">
-					<ul>
-						<li class="dropdown">
-							<a href="" > HOMBREsss </a>
-							<div class="dropdown-content">
-								<a> CAMISETAS </a>
-								<a> PANTALONES </a>
-								<a> ZAPATILLAS </a>
-							</div>
-						</li>
-						<li class="dropdown">
-							<a href="" > MUJER </a>
-							<div class="dropdown-content">
-								<a> CAMISETAS </a>
-								<a> PANTALONES </a>
-								<a> ZAPATILLAS </a>
-							</div>
-						</li>
-						<li class="dropdown">
-							<a href="" > NIÑOS </a>
-							<div class="dropdown-content">
-								<a> CAMISETAS </a>
-								<a> PANTALONES </a>
-								<a> ZAPATILLAS </a>
-							</div>
-						</li>
-						<li class="dropdown">
-							<a href="" > COMPLEMENTOS </a>
-							<div class="dropdown-content">
-								<a> CAMISETAS </a>
-								<a> PANTALONES </a>
-								<a> ZAPATILLAS </a>
-							</div>
-						</li>
-					</ul>
-				</nav>
-			</header><!-- /header -->
-
-			<div id="inicio">
-				<section id="slider1">
-					<img id="globoRojo" class="globosCarousel" src="img/ropa-banner.jpg">
-					<img id="globoAzul" class="globosCarousel" src="img/accesorios-banner.jpg">
-					<img id="globoVerde" class="globosCarousel" src="img/suplementos-banner.png">
-				</section>
+        <div id="inicio">
+            <section id="slider1">
+                <img id="globoRojo" class="globosCarousel" src="img/ropa-banner.jpg">
+                <img id="globoAzul" class="globosCarousel" src="img/accesorios-banner.jpg">
+                <img id="globoVerde" class="globosCarousel" src="img/suplementos-banner.png">
+            </section>
 
 
-				<section class="informacion">
-					<h1> <b> SURU VOLADOR  </b></h1>
-					<p> Son las mejores del mercado! Le quitamos el jack al iphone para venderte un adaptador solamente por 49.95€ que puedes añadirlo al boton de carga y funciona como jack, carga y a demas... tiene un boton para ir a inicio! (El X no tiene boton, regalamos este boton!!) Solo por 49.95€.</p>
-				</section>
+            <section class="informacion">
+                <h1> <b> SURU VOLADOR  </b></h1>
+                <p> Son las mejores del mercado! Le quitamos el jack al iphone para venderte un adaptador solamente por 49.95€ que puedes añadirlo al boton de carga y funciona como jack, carga y a demas... tiene un boton para ir a inicio! (El X no tiene boton, regalamos este boton!!) Solo por 49.95€.</p>
+            </section>
 
-				<section id="slider2">
-					<img id="pelotaRoja" class="pelotasCarousel" src="img/globoAzul.jpg">
-					<img id="pelotaAzul" class="pelotasCarousel" src="img/globoRojo.jpg">
-					<img id="pelotaBlanca" class="pelotasCarousel" src="img/globoVerde.jpg">
-				</section>
+            <section id="slider2">
+                <img id="pelotaRoja" class="pelotasCarousel" src="img/globoAzul.jpg">
+                <img id="pelotaAzul" class="pelotasCarousel" src="img/globoRojo.jpg">
+                <img id="pelotaBlanca" class="pelotasCarousel" src="img/globoVerde.jpg">
+            </section>
 
-				<section id="info1" class="informacion">
-					<h1> Compranos esto s'hurmano que esta to gupao </h1>
-					<p> Son las mejores del mercado! Le quitamos el jack al iphone para venderte un adaptador solamente por 49.95€ que puedes añadirlo al boton de carga y funciona como jack, carga y a demas... tiene un boton para ir a inicio! (El X no tiene boton, regalamos este boton!!) Solo por 49.95€.</p>
-				</section>
+            <section id="info1" class="informacion">
+                <h1> Compranos esto s'hurmano que esta to gupao </h1>
+                <p> Son las mejores del mercado! Le quitamos el jack al iphone para venderte un adaptador solamente por 49.95€ que puedes añadirlo al boton de carga y funciona como jack, carga y a demas... tiene un boton para ir a inicio! (El X no tiene boton, regalamos este boton!!) Solo por 49.95€.</p>
+            </section>
 
-				<script src="js/carousel.js"></script>
-			</div>
-			<footer>
-				<h1> Contacta con nosotros </h1>
-			</footer>
+            <script src="js/carousel.js"></script>
+        </div>
+        <footer>
+            <h1> Contacta con nosotros </h1>
+        </footer>
 
-		</body>
-	</html>
-<?php
-?>
+    </body>
+</html>
